6ef5b94707046c4eed4a761883e16780eb4f99e0
Angéline <anje.jiro@gmail.com>
2023-03-01 16:42:42 +0100

    Setup connection à la base de donnés

--- model/connexion.php
+++ model/connexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=blog; charset=utf8', 'root');
+    $db = new PDO('mysql:host=localhost;dbname=bouquine; charset=utf8', 'root');
 } catch (PDOException $e) {
     echo 'Erreur:' . $e->getMessage();
 }
