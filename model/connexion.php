90ae0c0c5cfd8e98f8209dbf09a4d51242fd86b7
Angéline <anje.jiro@gmail.com>
2023-03-02 17:24:39 +0100

    Update des fichiers pour CRUD

--- model/connexion.php
+++ model/connexion.php
@@ -1,6 +1,9 @@
 <?php
+function connection() {
 try {
     $db = new PDO('mysql:host=localhost;dbname=bouquine; charset=utf8', 'root');
 } catch (PDOException $e) {
-    echo 'Erreur:' . $e->getMessage();
-}
+    echo 'Erreur:' . $e->getMessage() . '<br>';
+}}
+
+?>
