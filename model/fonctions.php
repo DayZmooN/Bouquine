b4e805af8777d9fb0cbcfb0567430e8a5930f9dc
Angéline <anje.jiro@gmail.com>
2023-03-03 10:13:50 +0100

    Function CRUD start

--- model/fonctions.php
+++ model/fonctions.php
@@ -1,6 +1,11 @@
 <?php
 
-
-
+//Récupère tout de la base de données
+function getAll(){
+    $con = connection();
+    $req = "SELECT * from `admin`,`book`,`category`,`genre`,`genre_book`,`loan`,`user`;";
+    $rows = $con->query($req);
+    return $rows;
+}
 
 ?>
