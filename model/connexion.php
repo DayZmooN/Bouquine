ffea13ef032bad70ac57a6c668f6bc5af4647d35
DayZmooN <karim-du01@live.fr>
2023-03-06 09:35:20 +0100

    création de branch

--- model/connexion.php
+++ model/connexion.php
@@ -1,9 +1,11 @@
 <?php
-function connection() {
-try {
-    $db = new PDO('mysql:host=localhost;dbname=bouquine; charset=utf8', 'root');
-} catch (PDOException $e) {
-    echo 'Erreur:' . $e->getMessage() . '<br>';
-}}
-
-?>
+function connection()
+{
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=bouquine', 'root', '');
+        $db->exec('SET NAMES "UTF8"');
+    } catch (PDOException $e) {
+        echo 'Erreur: ' . $e->getMessage() . '<br>';
+        die();
+    }
+}
