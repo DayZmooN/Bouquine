e64c78e7e00b03dead3425cbe3ee2d8cf050899c
orikham <orikham@hotmail.fr>
2023-03-06 16:57:58 +0100

    1 er commit dimitri

---
+++ admin/header-admin.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
+    <title>dashboard</title>
+    <link rel="stylesheet" href="../css/style-admin.css">
+
+
+</head>
+<body>
+<header id="headerDashboard" >
+    <div id="titrePage">
+        <h1>dashboard</h1>
+    </div>
+
+    <div id="infoAdmin">
+        <img class="notif" src="../image/boutonNotificationDashboard.png" alt="bouton cloche de notifications">
+        <img src="../image/adminDashboard.png" alt="icone administrateur">
+        <p>Admin</p>
+        <img src="../image/deconnexionDashboard.png" alt="bouton de deconnexion" color-size="red">
+    </div>
+</header>
+
+<section id="sectionDashboard">
+    <div id="logo">
+        <img class="bouquine"  src="../image/logoAdmin.png" alt="logo du site bouquine">
+    </div>
+    <div id="menu-admin">
+        <nav>
+            <ul>
+                <li><img src="../image/tableauDeBord.png" alt="icone du dashboard"> <a href="#">Tableau de bord</a></li>
+                <li><img src="../image/articleDashboard.png" alt="icone articles du dashboard"><a href="#">Articles</a></li>
+                <li><img src="../image/catégorieDashboard.png" alt="icone catégorie du dashboard"><a href="#">Catégories</a></li>
+                <li><img src="../image/genresDashboard.png" alt="icone genres du dashboard"><a href="#">Genre</a></li>
+                <li><img src="../image/userDashboard.png" alt="icone user du dashboard"><a href="#">Users</a></li>
+                <li><img src="../image/newsLetterDashboard.png" alt="icone newsletter du dashboard"><a href="#">Newsletter</a></li>
+            </ul>
+        </nav>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
