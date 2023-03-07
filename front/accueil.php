5e913726dcf193ee98bbf7eed0863fb2ae9ff015
najia01 <validress@outlook.fr>
2023-03-07 22:38:27 +0100

    accueil.php

---
+++ front/accueil.php
@@ -0,0 +1,87 @@
+</html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bouquine</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- /<link rel="stylesheet" href="../css/bootstrap.min.css"> -->
+</head>
+
+<body>
+    <!-- SECTION 1 EN TETE  -->
+    <main>
+        <section class="parallax-section">
+            <div class="parallax parallax1">
+                <h1>Bouquine</h1>
+                <p id="explain"> C'est un lieu de savoir et de <br> découverte qui abrite
+                    une vaste <br>collection de documents<br> imprimés
+                    ainsi que des<br> ressources numériques pour<br> répondre
+                    à tous vos besoins de<br> recherche et de lecture.</p>
+            </div>
+        </section>
+
+
+        <section class="populaire">
+            <div class="titre1">
+                <h2 class="popular">Les plus populaires </h2>
+            </div>
+            <div class="container">
+                <div class="item">
+                    <a href="#"><img src="../image/star wars.jpg"></a>
+                    <p class="title">STAR WARS</p><br>
+                    <p class="author">Mike CHEN</p>
+                </div>
+                <div class="item"><a href="#"><img src="../image/romance.jpg"></a>
+                    <p class="title">ROMANCE</p><br>
+                    <p class="author">Arnaud CATHERINE</p>
+                </div>
+                <div class="item"><a href="#"><img src="../image/assasin royal.jpg"></a>
+                    <p class="title">L'ASSASIN ROYAL </p><br>
+                    <p class="author">Robin HOBB</p>
+                </div>
+
+                <div class="item"><a href="#"><img src="../image/dark romance.jpg"></a>
+                    <p class="title">DARK ROMANCE</p><br>
+                    <p class="author">Péneloppe DOUGLAS</p>
+                </div>
+
+                <div class="item"><img src="../image/circe.jpg"></a>
+                    <p class="title">CIRCE</p><br>
+                    <p class="author">Madeleine MILLER</p>
+                </div>
+
+                <div class="item"><a href="#"><img src="../image/dans ma tete.jpg"></a>
+                    <p class="title">J'AI TOUT DANS MA TETE</p><br>
+                    <p class="author">Rachel ARDITI</p>
+                </div>
+
+                <div class="item"><a href="#"><img src="../image/aime moi.jpg"></a>
+                    <p class="title">AIME MOI </p><br>
+                    <p class="author">Morgane MONTCOMBLE</p>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="../image/crepuscule nouv1.jpg"></a>
+                    <p class="title">CREPUSCULE</p><br>
+                    <p class="author">Philippe CLAUDEL</p>
+                </div>
+
+            </div>
+
+
+
+        </section>
+    </main>
+
+
+
+
+
+
+
+</body>
+
+</html>
