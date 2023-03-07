bd649819f1504b5179f5ea50dde348fc3e455beb
orikham <orikham@hotmail.fr>
2023-03-07 16:39:22 +0100

    creation contenus header-admin et article-admin,

---
+++ admin/article-admin.php
@@ -0,0 +1,36 @@
+
+
+
+<!-- <link rel="stylesheet" href="../css/style-admin.css"> -->
+
+<?php 
+    include './header-admin.php';
+
+?>
+
+
+
+
+<main class="multitaches">
+    
+
+        <h1>menus articles</h1>
+    <div id="menu-article">
+        <form action="" method="post">
+            <input class="recherche" type="search" name="recherche" style="width: 300px; height: 80px;" placeholder="rechercher directement un ouvrage" >
+            <img src="../image/loupe.png" alt="loupe clicable pour lancer la recherche" title="lancer la recherche">
+        </form>
+        <a href="#">ajouter de nouveaux livres</a>
+
+
+    </div>
+
+
+
+
+</main>
+
+
+
+
+
