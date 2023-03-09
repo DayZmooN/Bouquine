01ac4d030f4712f781678a6bd4917fca12daf243
orikham <orikham@hotmail.fr>
2023-03-09 09:39:42 +0100

    creation dashboard et article

---
+++ admin/genre.php
@@ -0,0 +1,31 @@
+<?php 
+    include './header-admin.php';
+
+?>
+
+
+
+
+
+    
+
+    <h1 class="multiTitre">menus genres</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</main>
+</div>
+<!-- </body>
+
+</html> -->
--- admin/header-admin.php
+++ admin/header-admin.php
@@ -21,11 +21,11 @@
             <div id="menu-admin">
                 <nav>
                     <ul>
-                        <li><img src="../image/tableauDeBord.png" alt="icone du dashboard"> <a href="#">Tableau de bord</a></li>
+                        <li><img src="../image/tableauDeBord.png" alt="icone du dashboard"> <a href="./dashboard.php">Tableau de bord</a></li>
                         <li><img src="../image/articleDashboard.png" alt="icone articles du dashboard"><a href="./article-admin.php">Articles</a></li>
-                        <li><img src="../image/catégorieDashboard.png" alt="icone catégorie du dashboard"><a href="#">Catégories</a></li>
-                        <li><img src="../image/genresDashboard.png" alt="icone genres du dashboard"><a href="#">Genre</a></li>
-                        <li><img src="../image/userDashboard.png" alt="icone user du dashboard"><a href="#">Users</a></li>
+                        <li><img src="../image/catégorieDashboard.png" alt="icone catégorie du dashboard"><a href="./categorie.php">Catégories</a></li>
+                        <li><img src="../image/genresDashboard.png" alt="icone genres du dashboard"><a href="./genre.php">Genre</a></li>
+                        <li><img src="../image/userDashboard.png" alt="icone user du dashboard"><a href="./user.php">Users</a></li>
                         <li><img src="../image/newsLetterDashboard.png" alt="icone newsletter du dashboard"><a href="#">Newsletter</a></li>
                     </ul>
                 </nav>
@@ -33,6 +33,8 @@
     
     </aside>
 
+
+<div class="test">
     <header id="headerDashboard">
            
                 <img class="notif" src="../image/adminDashboard.png" alt="icone administrateur">
@@ -56,6 +58,6 @@
 
 
 
-</body>
+    </body>
 
 </html>
