1c4bf77ef31059adbabcd807b14057c6556a6fd9
najia01 <validress@outlook.fr>
2023-03-09 16:57:42 +0100

    footer new

---
+++ front/footer-front.php
@@ -0,0 +1,43 @@
+<?php
+require_once './header-front.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bouquine</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <footer>
+        <section id="end">
+            <div class="footer">
+
+            </div class="logoB">
+            <img src="../image/b.png" alt="logo Bouquine B">
+            </div>
+            <div class="pol">
+                <a href="../doc/Model__CGU__.docx">CGU</a>
+            </div>
+            <div class="politique">
+                <a href="../doc/Politique-de-confidentialité.docx">Politique de confidentialité</a>
+            </div>
+            <div class="réseaux">
+                <a href="#"><img src="../image/facebook.png"></a>
+                <a href="#"><img src="../image/instagram.png"></a>
+                <a href="#"><img src="../image/twitter.png"></a>
+                <a href="#"><img src="../image/linkedin.png"></a>
+            </div>
+            <div class="copyright">Copyright&copy;2023 Bouquine</div>
+
+            </div>
+        </section>
+
+   </footer>
+</body>
+
+</html>
