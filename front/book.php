2e495162c0a69a47316b42b0455ef49e70d29e9f
najia01 <validress@outlook.fr>
2023-03-10 13:28:51 +0100

    ok

--- front/book.php
+++ front/book.php
@@ -1,5 +1,6 @@
 <?php
 require_once './header-front.php';
+require_once './footer-front.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,48 +14,58 @@ require_once './header-front.php';
 
 <body>
     <section id="books">
+        <div class="contain">
+            <div class="resume">
+                <img id="circe" src="../image/circe.jpg" alt="livre Circé de Madeleine Miller">
 
-        <div class="resume">
-            <img src="../image/circe.jpg" alt="livre Circé de Madeleine Miller">
-
+            </div>
+            <div class="resum">
+                <h3>Circé poche 2 Mai 2019<br>
+                    Madeleine MILLER<br>
+                    Edition pocket
+                </h3>
+                <p>
+                    Fruit des amours d'un dieu et d'une mortelle, Circé la nymphe grandit
+                    parmi les divinités de l'Olympe. Mais son caractère étonne. Détonne. On la dit sorcière, parce qu'elle aime changer les choses.
+                    Plus humaine que céleste
+                    , parce qu'elle est sensible. En l'exilant sur une île déserte, comme le fut jadis Prométhée pour avoir trop aimé les hommes, ses pairs ne lui
+                    ont-ils pas plutôt rendu service ? Là, l'immortelle peut choisir qui elle est. Demi-déesse, certes, mais femme avant tout .......</p>
+            </div>
         </div>
-        <div class="resum">
-            <h3>Circé poche 2 Mai 2019
-                Madeleine MILLER
-                Edition pocket
-            </h3>
-            <p>Circé Poche-2 MAi 2019
-                de Madeline Miller
-                Edition Pocket
-
-                Fruit des amours d'un dieu et d'une mortelle, Circé la nymphe grandit
-                parmi les divinités de l'Olympe. Mais son caractère étonne. Détonne. On la dit sorcière, parce qu'elle aime changer les choses.
-                Plus humaine que céleste
-                , parce qu'elle est sensible. En l'exilant sur une île déserte, comme le fut jadis Prométhée pour avoir trop aimé les hommes, ses pairs ne lui
-                ont-ils pas plutôt rendu service ? Là, l'immortelle peut choisir qui elle est. Demi-déesse, certes, mais femme avant tout .......</p>
-        </div>
-
         <div class="lus">
+            <h3 class="read">Les utilisateurs ont également lu</h3>
             <a href="#"><img src="../image/le chant d achille.jpg" alt="le chant d achille de MAdeleine Miller">
+                <p class="titl1">Le chant d'Achille </p>
+                <p class="author1">Madeline Miller </p>
+
                 <a href="#"><img src="../image/assasin royal.jpg" alt="L'assassin royal de Robin Hobb ">
+                    <p class="titl1">L'assassin royal</p>
+                    <p class="author1">Robin HOBB </p>
+
                     <a href="#"><img src="../image/la-croisade-eternelle.jpg" alt="la croisade eternelle de Victor Fleury">
+                        <p class="titl1">La croisade éternelle </p>
+                        <p class="author1">Victor FLEURY </p>
+
                         <a href="#"><img src="../image/sistine.jpg" alt="Sixtine de Caroline Vermalle">
+                            <p class="titl1">Sixtine </p>
+                            <p class="author1">Caroline VERMALLE </p>
         </div>
+
         <div class="description">
 
-            <h3>Descrption du produit</h3>
-            <h4>Biographie de l'auteur</h4>
-            <p>Madeline Miller est passionnée par la Grèce antique.
+            <h3 class="describe">Descrption du produit</h3>
+            <h4 class="bio">Biographie de l'auteur</h4>
+            <p class="miller">Madeline Miller est passionnée par la Grèce antique.
                 Après des études d'Histoire et de Littérature classique
-                à Yale, elle devient professeur de grec ancien, de latin,
+                à Yale, elle devient professeur de grec ancien, de latin,<br>
                 sans oublier les cours qu'elle anime sur l'œuvre
                 de Shakespeare. Le Chant d'Achille est son premier
                 roman, suivi de Circé.</p>
         </div>
         <div class="list">
-            <h4>Détails du produits</h4>
+            <h4 class="detail">Détail du produit</h4>
 
-            <ul>
+            <ul class="liste">
                 <li> Éditeur Pocket (12 janvier 2023)</li>
                 <li>Langue Français</li>
                 <li>Poche : 480 pages</li>
@@ -67,12 +78,31 @@ require_once './header-front.php';
             </ul>
         </div>
 
-        <h3>Disponibles dans la même édition</h3>
+        <h3 class="dispo">Disponibles dans la même édition</h3>
+
         <div class="livr">
-            
+
+            <div class="item3">
+                <a href="#"><img src="../image/star wars.jpg" alt="Star wars de Mike CHEN"></a>
+                <p class="title2">STAR WARS</p><br>
+                <p class="author2">Mike CHEN</p>
+            </div>
+
+            <div class="item3"><a href="#"><img src="../image/romance.jpg" alt="Romance d'Arnaud CATHERINE "></a>
+                <p class="title2">ROMANCE</p><br>
+                <p class="author2">Arnaud CATHERINE</p>
+            </div>
+
+            <div class="item3"><a href="#"><img src="../image/assasin royal.jpg" alt="L'assasin Royal de Robin HOBB "></a>
+                <p class="title2">L'ASSASIN ROYAL </p><br>
+                <p class="author2">Robin HOBB</p>
+            </div>
+
+
+
 
         </div>
-
+        </div>
     </section>
 </body>
 
