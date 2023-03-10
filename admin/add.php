43c624406868ea47eba20aeee9ea2776981a05aa
orikham <orikham@hotmail.fr>
2023-03-10 10:08:42 +0100

    maj du 10/03 matin

--- admin/add.php
+++ admin/add.php
@@ -11,6 +11,7 @@
 <body>
     <?php
     require_once '../connexion.php';
+    include './header-admin.php';
 
     if (isset($_POST['submit'])) {
         $ISBN = addslashes($_POST['ISBN']);
@@ -32,39 +33,128 @@
         exit();
     }
     ?>
-    <form action="#" method="POST">
-        <label for="ISBN">ISBN</label>
-        <input type="text" name="ISBN" id="ISBN">
-        <br>
-        <label for="image">Cover</label>
-        <input type="text" name="image" id="image">
-        <br>
-        <label for="title">Titre</label>
-        <input type="text" name="title" id="title">
-        <br>
-        <label for="author">Auteur</label>
-        <input type="text" name="author" id="author">
-        <br>
-        <label for="editor">Éditeur</label>
-        <input type="text" name="editor" id="editor">
-        <br>
-        <label for="collection">Collection</label>
-        <input type="text" name="collection" id="collection">
-        <br>
-        <label for="publication_date">Date de publication :<br>Année - m.m-j.j</label>
-        <input type="text" name="publication_date" id="publication_date">
-        <br>
-        <label for="genre">Genre</label>
-        <input type="text" name="genre" id="genre">
-        <br>
+    <h1 class="multiTitre">formulaire ajout de livre</h1>
+
+    <form id="formulaire" action="#" method="POST">
+        <div class="titre-auteur">
+
+            <label for="title"></label>
+            <input type="text" name="title" id="title" placeholder="TITRE">
+
+            <label for="author"></label>
+            <input type="text" name="author" id="author" placeholder="Auteur">
+
+            <label for="ISBN"></label>
+            <input type="text" name="ISBN" id="ISBN" placeholder="ISBN" >
+
+        </div>
+
+        <div class="edition-date">
+
+            
+                <label for="editor"></label>
+                <input type="text" name="editor" id="editor" placeholder="Éditeur">
+            
+
+           
+            <input class="date" type="date" name="publication_date" id="publication_date" placeholder="Éditeur">
+                <label for="publication_date">Publication </label>
+            
+            
+
+        </div>
+
+        <div class="select">
         <label for="id_category">Catégorie</label>
-        <input type="number" name="id_category" id="id_category">
-        <br>
+        <select name="id_category" id="id_category">
+            <option value="BD">b.d</option>
+            <option value="Comics">comics</option>
+            <option value="Documentaire">documentaire</option>
+            <option value="Jeunesse">Jeunesse</option>
+            <option value="Mangas">mangas</option>
+            <option value="Poésie">poésie</option>
+            <option value="Romans">romans</option>
+            <option value="Théatre">théatre</option>
+        </select>
+
+        <label for="genre">Genre</label>
+        <select type="text" name="genre" id="genre">
+            <option value="action">action</option>
+            <option value="aventure">aventure</option>
+            <option value="drame">drame</option>
+            <option value="fantasie">fantasie</option>
+            <option value="historique">historique</option>
+            <option value="horreur">horreur</option>
+            <option value="policier">policier</option>
+            <option value="romance">romance</option>
+            <option value="science-fiction">science-fiction</option>
+            <option value="thriller">thriller</option>
+        </select>
+
+        <label for="collection">Collection</label>
+        <select type="text" name="collection" id="collection" placeholder="">
+            <option value="collection1">collection 1</option>
+            <option value="collection2">collection 2</option>
+            <option value="collection3">collection 3</option>
+            <option value="collection4">collection 4</option>
+            <option value="collection5">collection 5</option>
+        </select>
+
+            
+
+        </div>
+
+        <div>
+
+        
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        <label for="image">Couverture</label>
+        <input type="file" name="image" id="image" placeholder="">
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         <label for="summary">Résumé</label>
         <input type="text" name="summary" id="summary">
-        <br>
-        <button type="submit" name="submit" value="Post">Submit</button>
+        
+        <button  type="submit" name="submit" value="Post">Submit</button>
     </form>
+
+    </main>
+</div>
+
 </body>
 
 </html>
--- admin/header-admin.php
+++ admin/header-admin.php
@@ -12,41 +12,39 @@
 </head>
 
 <body>
-        
 
-        <aside id="side-bar">
-            <div id="logo">
-                <img src="../image/logoAdmin.png" alt="logo du site bouquine">
-            </div>
-            <div id="menu-admin">
-                <nav>
-                    <ul>
-                        <li><img src="../image/tableauDeBord.png" alt="icone du dashboard"> <a href="./dashboard-admin.php">Tableau de bord</a></li>
-                        <li><img src="../image/articleDashboard.png" alt="icone articles du dashboard"><a href="./article.php">Articles</a></li>
-                        <li><img src="../image/catégorieDashboard.png" alt="icone catégorie du dashboard"><a href="./categorie.php">Catégories</a></li>
-                        <li><img src="../image/genresDashboard.png" alt="icone genres du dashboard"><a href="./genre.php">Genre</a></li>
-                        <li><img src="../image/userDashboard.png" alt="icone user du dashboard"><a href="./user.php">Users</a></li>
-                        <li><img src="../image/newsLetterDashboard.png" alt="icone newsletter du dashboard"><a href="#">Newsletter</a></li>
-                    </ul>
-                </nav>
-            </div>
-    
+
+    <aside id="side-bar">
+        <div id="logo">
+            <img src="../image/logoAdmin.png" alt="logo du site bouquine">
+        </div>
+        <div id="menu-admin">
+            <nav>
+                <ul>
+                    <li><img src="../image/tableauDeBord.png" alt="icone du dashboard"> <a href="./dashboard-admin.php">Tableau de bord</a></li>
+                    <li><img src="../image/articleDashboard.png" alt="icone articles du dashboard"><a href="./article.php">Articles</a></li>
+                    <li><img src="../image/catégorieDashboard.png" alt="icone catégorie du dashboard"><a href="./categorie.php">Catégories</a></li>
+                    <li><img src="../image/genresDashboard.png" alt="icone genres du dashboard"><a href="./genre.php">Genre</a></li>
+                    <li><img src="../image/userDashboard.png" alt="icone user du dashboard"><a href="./user.php">Users</a></li>
+                    <li><img src="../image/newsLetterDashboard.png" alt="icone newsletter du dashboard"><a href="#">Newsletter</a></li>
+                </ul>
+            </nav>
+        </div>
+
     </aside>
 
 
-<div class="test">
-    <header id="headerDashboard">
-           
-                <img class="notif" src="../image/adminDashboard.png" alt="icone administrateur">
-                <p>Admin</p>
-                <img class="notif" src="../image/boutonNotificationDashboard.png" alt="bouton cloche de notifications" title="voir vos notifications">
-                <img class="notif" src="../image/deconnexionDashboard.png" alt="bouton de deconnexion" title="vous deconnectez">
-           
-    </header> 
-    
-    <main class="multitaches">
+    <div class="test">
+        <header id="headerDashboard">
 
-    
+            <img class="notif" src="../image/adminDashboard.png" alt="icone administrateur">
+            <p>Admin</p>
+            <img class="notif" src="../image/boutonNotificationDashboard.png" alt="bouton cloche de notifications" title="voir vos notifications">
+            <img class="notif" src="../image/deconnexionDashboard.png" alt="bouton de deconnexion" title="vous deconnectez">
+
+        </header>
+
+        <main class="multitaches">
 
 
 
@@ -54,10 +52,3 @@
 
 
 
-
-
-
-
-    </body>
-
-</html>
