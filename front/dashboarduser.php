067a2a50077b629c2b631d3167d3b170240062db
najia01 <validress@outlook.fr>
2023-03-12 00:03:49 +0100

    dashboard

---
+++ front/dashboarduser.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bouquine</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <section class="dash-user">
+        <div class="dashboard-container">
+            <h2>Mon tableau de bord </h2>
+            <div class="info-user">
+                <img src="../image/user1.png" alt="icône user">
+                <div class="details">
+                    <h3>Nom et prénom</h3>
+                    <p>Email : cocotte@gmail.com</p>
+                    <p>Téléphone : 06.86.36.56.32</p>
+                </div>
+            </div>
+            <div class="actions">
+                <a href="#">Emprunter un livre</a>
+                <a href="#">Voir la liste de mes emprunts</a>
+                <a href="#">Modifier mes informations personnelles</a>
+                <a href="#">Supprimer mon compte</a>
+                <a href="#">Déconnexion</a>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
