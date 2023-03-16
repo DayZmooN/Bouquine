697ab8fde0ccd1f51d7cf2fa037db10c13f448ed
Angéline <anje.jiro@gmail.com>
2023-03-16 11:15:55 +0100

    create back space for less conflict (hopefully)

--- admin/addeditgenre.php
+++ admin/addeditgenre.php
@@ -1,21 +1,6 @@
 <?php
 session_start();
 include './header-admin.php';
-
-try {
-    require_once '../auth.php';
-
-    $req = $db->prepare("INSERT INTO `genre` (`libel_genre`, `genre_slug`) VALUES (:libel_genre, :genre_slug)");
-    $req->bindValue(":libel_genre", $_POST["libel_genre"], PDO::PARAM_STR);
-    $req->bindValue(":genre_slug", $_POST["genre_slug"], PDO::PARAM_STR);
-    $req->execute();
-    $_SESSION["success"] = "votre genre a bien été crée";
-    header('location: ./genre.php');
-} catch (PDOException $e) {
-    $_SESSION["error"] = "Votre Genre n'a pas été crée";
-    header('Location: ./genre.php');
-    exit();
-}
 ?>
 <h1 class="multiTitre">modifications genres</h1>
 
