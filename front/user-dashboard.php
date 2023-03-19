9a4cefccea7e2265d002ec24d1b1aea7fff0a327
najia01 <validress@outlook.fr>
2023-03-19 16:04:31 +0100

    ok inscr

--- front/user-dashboard.php
+++ front/user-dashboard.php
@@ -5,7 +5,11 @@ require_once './header-front.php';
 <html>
 
 <head>
-  <title>Tableau de bord utilisateur </title>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Tableau de bord utilisateur </title>
   <link rel="stylesheet" href="../css/style.css">
 </head>
 
