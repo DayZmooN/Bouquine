79e578fdb18b9c4929431cac0544897d704ff081
DayZmooN <karim-du01@live.fr>
2023-03-20 16:56:18 +0100

    commit update header

--- front/header-front.php
+++ front/header-front.php
@@ -13,12 +13,12 @@ require_once './connect.php'
     <!-- HEADER -->
     <header>
         <nav class="header-nav">
-        <div id="close-menu">X</div>
+            <div id="close-menu">X</div>
             <div class="logo">
                 <a href="../front/accueil.php"><img src="../image/logo1.png" alt="logo bouquine "></a>
             </div>
-            
-           
+
+
 
             <ul class="menu">
                 <li><a href="./catalogue.php">Catalogue</a></li>
@@ -43,27 +43,27 @@ require_once './connect.php'
                         <button id="search-button" type="submit">Rechercher</button>
                     </form>
                 </li>
-           
-        
 
-        <?php if (isset($_SESSION['user'])) { ?>
-            <a href="./user-dashboard.php" class="img-user">
-                <img class="img-user" src="../image/user.png" alt="image">
-                <span class="nom"><?php echo $_SESSION['user']['username']; ?></span>
-            </a>
-            <form action="../model/deconnexion.php" method="post">
-                <button type="submit" name="logout">Déconnexion</button>
-            </form>
-        <?php }
-        if (!isset($_SESSION['user'])) { ?>
 
-            </a>
-            <a href="./connexion.php" class="img-user">
-                <input id="user-connect" type="button" value="Connexion">
-            </a>
-        <?php } ?>
-        </ul>
-        </div>
+
+                <?php if (isset($_SESSION['user'])) { ?>
+                    <a href="./user-dashboard.php" class="img-user">
+                        <img class="img-user" src="../image/user.png" alt="image">
+                        <span class="nom"><?php echo $_SESSION['user']['username']; ?></span>
+                    </a>
+                    <form action="../model/deconnexion.php" method="post">
+                        <input id="user-connect" type="submit" name="logout" value="Déconnexion">
+                    </form>
+                <?php }
+                if (!isset($_SESSION['user'])) { ?>
+
+                    </a>
+                    <a href="./connexion.php" class="img-user">
+                        <input id="user-connect" type="submit" value="Connexion">
+                    </a>
+                <?php } ?>
+            </ul>
+            </div>
         </nav>
         <div id="burger"><img src="../image/burger.png" alt="picto menu burger"></div>
     </header>
