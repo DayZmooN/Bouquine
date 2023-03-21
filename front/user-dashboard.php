9d8cb19c2aa2da907fbd6c811e4edf844a9309e2
DayZmooN <karim-du01@live.fr>
2023-03-21 16:20:01 +0100

    delete file

--- front/user-dashboard.php
+++ front/user-dashboard.php
@@ -11,19 +11,6 @@ if (!isset($_SESSION["user"])) {
 //on inclut le header 
 ?>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tableau de bord utilisateur </title>
-    <link rel="stylesheet" href="../css/style.css">
-  </head>
-
 <body>
 
   <div class="dashboard1">
