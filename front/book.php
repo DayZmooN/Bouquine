4b4a5a737232dce43bf944fb330a68d54114e9ae
Angéline <anje.jiro@gmail.com>
2023-03-23 08:32:03 +0100

    Modal réservation

--- front/book.php
+++ front/book.php
@@ -3,36 +3,42 @@ require_once './header-front.php';
 require_once './connect.php';
 require_once './footer-front.php';
 
-$id_user = $_SESSION["user"]["id"];
 $id = $_GET['id'];
 $query = $db->prepare('SELECT `id_book`, `ISBN`, `image`, `title`, `author`, `editor`, `collection`, `publication_date`, `genre`, `id_category`, `summary`, `status` FROM `book` WHERE `id_book` = :id');
 $query->bindParam('id', $id, PDO::PARAM_INT);
 $query->execute();
 $result = $query->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result as $book)
 
+    if (isset($_POST['rent'])) {
+        $id_user = $_SESSION["user"]["id"];
+        $req = $db->prepare("SELECT `id_book`,`status` FROM `book` WHERE `id_book` = :id");
+        $req->bindParam(':id', $id, PDO::PARAM_INT);
+        $req->execute();
+        $checkStatus = $req->fetchAll(PDO::FETCH_ASSOC);
+        //fetch données, if (book.status = 0) → pas dispo, si 1 → dispo
 
-if (isset($_POST['rent'])) {
-    $req = $db->prepare("SELECT `id_book`,`status` FROM `book` WHERE `id_book` = :id");
-    $req->bindParam(':id', $id, PDO::PARAM_INT);
-    $req->execute();
-    $checkStatus = $req->fetchAll(PDO::FETCH_ASSOC);
-    //fetch données, if (book.status = 0) → pas dispo, si 1 → dispo
-
-    if ($checkStatus[0]['status'] == 0) {
-        die("Ce livre n'est pas disponible pour le moment.");
-    } else {
-        $loan_date = date('Y-m-d H:i:s');
-        $return_date = date('Y-m-d H:i:s', strtotime('+3 hours'));
-        $rent = $db->prepare("INSERT INTO loan (id_book, id_user, loan_date, return_date) VALUES (:id_book, :id_user, NOW(), '$return_date')");
-        $rent->bindParam(':id_book', $id, PDO::PARAM_INT);
-        $rent->bindParam(':id_user', $id_user, PDO::PARAM_INT);
-        $rent->execute();
-        $statusUpdate = $db->prepare("UPDATE `book` SET `status`='0' WHERE `id_book` = :id");
-        $statusUpdate->bindParam(':id', $id, PDO::PARAM_INT);
-        $statusUpdate->execute();
-        echo "Votre réservation a été enregistrée.";
+        if ($checkStatus[0]['status'] == 0) {
+            $_SESSION['message'] = "Ce livre n'est pas disponible pour le moment.";
+            header("Location: ./book.php?id=" . $book['id_book']);
+            exit();
+            unset($_SESSION['message']);
+        } else {
+            $loan_date = date('Y-m-d H:i:s');
+            $return_date = date('Y-m-d H:i:s', strtotime('+3 hours'));
+            $rent = $db->prepare("INSERT INTO loan (id_book, id_user, loan_date, return_date) VALUES (:id_book, :id_user, NOW(), '$return_date')");
+            $rent->bindParam(':id_book', $id, PDO::PARAM_INT);
+            $rent->bindParam(':id_user', $id_user, PDO::PARAM_INT);
+            $rent->execute();
+            $statusUpdate = $db->prepare("UPDATE `book` SET `status`='0' WHERE `id_book` = :id");
+            $statusUpdate->bindParam(':id', $id, PDO::PARAM_INT);
+            $statusUpdate->execute();
+            $_SESSION['message'] = "Votre réservation a été enregistrée.";
+            header("Location: ./book.php?id=" . $book['id_book']);
+            exit();
+            
+        }
     }
-}
 ?>
 
 <body>
@@ -84,7 +90,7 @@ if (isset($_POST['rent'])) {
                 <h3 class="describe">Description du produit</h3>
                 <h4 class="bio">Biographie de l'auteur</h4>
                 <p class="miller">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>
-                 Asperiores suscipit non atque ipsa ullam quos aperiam debitis libero, accusamus vitae a alias quidem exercitationem neque perferendis quibusdam provident qui impedit!</p>
+                    Asperiores suscipit non atque ipsa ullam quos aperiam debitis libero, accusamus vitae a alias quidem exercitationem neque perferendis quibusdam provident qui impedit!</p>
 
         </div>
         <div class="list">
@@ -94,7 +100,7 @@ if (isset($_POST['rent'])) {
                 <li>Langue Français</li>
                 <li>Poche: 480 pages</li>
                 <li>ISBN: <?= $book['ISBN'] ?></li>
-                <li>Poids de l'article : 236 g</li>
+                <li>Poids de l'book : 236 g</li>
                 <li>Dimensions: 10.9 x 2 x 17.8 cm
                 </li>
 
@@ -121,7 +127,15 @@ if (isset($_POST['rent'])) {
                 </div>
             <?php } ?>
         </div>
+        <div id="myModal" class="modal">
+                    <!-- Modal content -->
+                    <div class="modal-content">
+                        <span class="close">&times;</span>
+                        <p id="message"><?php echo $_SESSION['message']; ?></p>
+                    </div>
+                </div>
     </section>
 </body>
+<script src="../js/main.js"></script>
 
 </html>
